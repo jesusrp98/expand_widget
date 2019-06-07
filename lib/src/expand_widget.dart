93b2ee28e6529f099a9a1ebff241cc17234fffb3
Jesús Rodríguez <jesusrope98@outlook.com>
2019-06-07 14:20:36 +0200

    Improved expand widget

--- lib/src/expand_widget.dart
+++ lib/src/expand_widget.dart
@@ -1,31 +1,95 @@
 import 'package:flutter/material.dart';
-
-import 'expand_arrow.dart';
+import 'package:flutter/widgets.dart';
 
 class ExpandWidget extends StatefulWidget {
+  final Duration animationDuration;
   final Widget child;
 
-  ExpandWidget({this.child});
+  const ExpandWidget({
+    Key key,
+    this.animationDuration = const Duration(milliseconds: 350),
+    @required this.child,
+  }) : super(key: key);
 
   @override
   _ExpandWidgetState createState() => _ExpandWidgetState();
 }
 
-class _ExpandWidgetState extends State<ExpandWidget> {
-  bool _isMinimized = true;
+class _ExpandWidgetState extends State<ExpandWidget>
+    with SingleTickerProviderStateMixin {
+  static final Animatable<double> _easeInTween =
+      CurveTween(curve: Curves.easeInOutCubic);
+  static final Animatable<double> _halfTween =
+      Tween<double>(begin: 0.0, end: 0.5);
 
-  void toggleContent() => setState(() => _isMinimized = !_isMinimized);
+  AnimationController _controller;
+  Animation<double> _heightFactor;
+  Animation<double> _iconTurns;
+
+  bool _isExpanded = false;
 
   @override
-  Widget build(BuildContext context) {
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: widget.animationDuration,
+      vsync: this,
+    );
+    _heightFactor = _controller.drive(_easeInTween);
+    _iconTurns = _controller.drive(_halfTween.chain(_easeInTween));
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _handleTap() {
+    setState(() {
+      _isExpanded = !_isExpanded;
+      _isExpanded ? _controller.forward() : _controller.reverse();
+    });
+  }
+
+  Widget _buildChildren(BuildContext context, Widget child) {
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: <Widget>[
-        if (!_isMinimized) widget.child,
-        ExpandArrow(
-          onTap: () => toggleContent(),
-        )
+        ClipRect(
+          child: Align(
+            alignment: Alignment.topCenter,
+            heightFactor: _heightFactor.value,
+            child: child,
+          ),
+        ),
+        Tooltip(
+          message: _isExpanded ? 'Show less' : 'Show more',
+          child: InkResponse(
+            child: Padding(
+              padding: const EdgeInsets.all(4),
+              child: RotationTransition(
+                turns: _iconTurns,
+                child: Icon(
+                  Icons.expand_more,
+                  size: null,
+                  color: Theme.of(context).textTheme.caption.color,
+                ),
+              ),
+            ),
+            onTap: _handleTap,
+          ),
+        ),
       ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _controller.view,
+      builder: _buildChildren,
+      child: widget.child,
     );
   }
 }
