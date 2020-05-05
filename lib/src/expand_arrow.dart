c249ee96adce16cd4618ed544553895c17ba50e9
jesusrp98 <jesusrope98@outlook.com>
2020-05-05 19:10:33 +0200

    Massive refactor of arrow

--- lib/src/expand_arrow.dart
+++ lib/src/expand_arrow.dart
@@ -1,5 +1,17 @@
 import 'package:flutter/material.dart';
 
+/// TODO
+enum ExpandArrowStyle {
+  ///
+  icon,
+
+  ///
+  text,
+
+  ///
+  both,
+}
+
 /// This widget is used in both [ExpandChild] & [ExpandText] widgets to show
 /// the hidden information to the user. It posses an [animation] parameter.
 /// Most widget parameters, such as [size] & [color] are customizable.
@@ -27,11 +39,17 @@ class ExpandArrow extends StatelessWidget {
   /// Defines arrow's size.
   final double size;
 
+  /// Icon that will be used instead of an arrow.
+  final IconData icon;
+
   /// Style of the displayed message.
   final TextStyle hintTextStyle;
 
   /// Defines arrow rendering style.
   final bool displayHintText;
+
+  /// TODO
+  final ExpandArrowStyle expandArrowStyle;
 
   const ExpandArrow({
     Key key,
@@ -42,13 +60,15 @@ class ExpandArrow extends StatelessWidget {
     @required this.onTap,
     this.color,
     this.size,
+    this.icon,
     this.hintTextStyle,
     this.displayHintText,
+    this.expandArrowStyle = ExpandArrowStyle.both,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final tooltipMessage = animation.value == 0
+    final tooltipMessage = animation.value <= 0.25
         ? collapsedHint ??
             MaterialLocalizations.of(context).collapsedIconTapHint
         : expandedHint ?? MaterialLocalizations.of(context).expandedIconTapHint;
@@ -56,36 +76,43 @@ class ExpandArrow extends StatelessWidget {
     final animatedArrow = RotationTransition(
       turns: animation,
       child: Icon(
-        Icons.expand_more,
+        icon ?? Icons.expand_more,
         color: color ?? Theme.of(context).textTheme.caption.color,
         size: size,
       ),
     );
 
-    return Padding(
-      padding: padding ?? EdgeInsets.all(4),
-      child: Tooltip(
-        message: tooltipMessage,
-        child: displayHintText == true
-            ? InkWell(
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    animatedArrow,
-                    const SizedBox(width: 8.0),
-                    Text(
-                      tooltipMessage,
-                      style: hintTextStyle,
-                    ),
-                    const SizedBox(width: 8.0),
-                  ],
+    return Tooltip(
+      message: tooltipMessage,
+      child: InkResponse(
+        containedInkWell: expandArrowStyle != ExpandArrowStyle.icon,
+        highlightShape: expandArrowStyle != ExpandArrowStyle.icon
+            ? BoxShape.rectangle
+            : BoxShape.circle,
+        child: Padding(
+          padding: padding ??
+              EdgeInsets.all(expandArrowStyle == ExpandArrowStyle.text ? 8 : 4),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              if (expandArrowStyle != ExpandArrowStyle.text) animatedArrow,
+              if (expandArrowStyle != ExpandArrowStyle.icon) ...[
+                const SizedBox(width: 2.0),
+                DefaultTextStyle(
+                  style: Theme.of(context).textTheme.subtitle2.copyWith(
+                        color: Theme.of(context).textTheme.caption.color,
+                      ),
+                  child: Text(
+                    tooltipMessage.toUpperCase(),
+                    style: hintTextStyle,
+                  ),
                 ),
-                onTap: onTap,
-              )
-            : InkResponse(
-                child: animatedArrow,
-                onTap: onTap,
-              ),
+                const SizedBox(width: 2.0),
+              ]
+            ],
+          ),
+        ),
+        onTap: onTap,
       ),
     );
   }
